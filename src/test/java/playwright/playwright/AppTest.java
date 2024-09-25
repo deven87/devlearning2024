d4fc8066205cae0b9150b52544ce887a8741c8f7
devendra <karayla.devendra@gmail.com>
2024-09-25 14:17:13 +0530

    added playwright basic test

--- src/test/java/playwright/playwright/AppTest.java
+++ src/test/java/playwright/playwright/AppTest.java
@@ -1,38 +1,34 @@
 package playwright.playwright;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.testng.annotations.Test;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+	
+	
+	@Test
+	public void runPlaywright() {
+		
+		Playwright playwright = Playwright.create();
+		
+		Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		
+		Page page = browser.newPage();
+		
+		page.navigate("https://www.intelligent.com");
+		
+		System.out.println(page.url());
+		
+		System.out.println(page.title());
+		
+	}
+	
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
