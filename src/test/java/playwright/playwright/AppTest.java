3f2d1993f715c8cb949ac7889b6d9a66d5d8a540
devendra <devendra.singh@intelligent.com>
2024-09-27 18:05:24 +0530

    playwright test

--- src/test/java/playwright/playwright/AppTest.java
+++ src/test/java/playwright/playwright/AppTest.java
@@ -1,5 +1,6 @@
 package playwright.playwright;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
@@ -8,6 +9,7 @@ import org.testng.annotations.Test;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Download;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
@@ -260,7 +262,7 @@ public class AppTest {
         
     }
     
-    @Test(description = "playwright table dynamic", enabled = true)
+    @Test(description = "playwright table dynamic", enabled = false)
     public void playwrightTestDynamicTable() {
         
         Playwright py = Playwright.create();
@@ -270,14 +272,234 @@ public class AppTest {
         // cookies and cache are not shared between different browser contexts
         Page p1 = bc.newPage();
                 
-        p1.navigate("https://netflix.com/in/");
+        p1.navigate("https://datatables.net/");
         
-        p1.locator("_react=component_name[attr = 'value']");
+        // get all the trs
+        Locator table = p1.locator("table#example tr");
         
+        // get scope for the whole table
+        List<String> innerText = table.locator(":scope").allInnerTexts();
+        
+        //innerText.forEach(str -> System.out.println(str));
+        
+        // scope only a given row based on rows text and then find locator in that row only
+        String cellValueForGivenRowHavingAText = table.locator(":scope", new Locator.LocatorOptions().setHasText("Ashton Cox")).locator(".dt-type-numeric").first().textContent();
+        
+        System.out.println(cellValueForGivenRowHavingAText);
         
     }
     
     
+    @Test(description = "playwright login state save", enabled = false)
+    public void playwrightTestLoginStateSave() {
+        
+        Playwright py = Playwright.create();
+        Browser browser = py.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        BrowserContext bc = browser.newContext();
+        
+        // cookies and cache are not shared between different browser contexts
+        Page p1 = bc.newPage();
+                
 
+        p1.navigate("https://app.resume.org/login/");
+              p1.fill("input[type='email']", "devendra.singh@intelligent.com");
+              p1.fill("input[type='password']", "Test@531");
+              p1.locator("#submit_button").click();
+              
+              try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+              
+              // store the login state in json file
+              bc.storageState(new BrowserContext.StorageStateOptions().setPath(Paths.get("applogin.json")));
+    }
+    
+    @Test(description = "playwright login state using", enabled = false)
+    public void playwrightTestLoginStateUsing() {
+        
+        Playwright py = Playwright.create();
+        Browser browser = py.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        
+        // use the previous saved login state json file here
+        BrowserContext bc = browser.newContext(new Browser.NewContextOptions().setStorageStatePath(Paths.get("applogin.json")));
+        
+        // cookies and cache are not shared between different browser contexts
+        Page p1 = bc.newPage();
+                
+
+        p1.navigate("https://app.resume.org/my-account/");
+        
+      
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+             }
+    
+    
+    @Test(description = "playwright alerts", enabled = false)
+    public void playwrightTestAlerts() {
+        
+        Playwright py = Playwright.create();
+        Browser browser = py.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        
+        BrowserContext bc = browser.newContext();
+        
+        Page p1 = bc.newPage();
+        
+        p1.onDialog(dialog -> {
+            
+            String text = dialog.message();
+            System.out.println(text);
+            dialog.accept("hello i am prompt message");
+            
+        });
+                
+
+        p1.navigate("https://demoqa.com/alerts");
+        
+    //    p1.locator("#alertButton").click();
+        
+       // p1.locator("#confirmButton").click();
+        
+        p1.locator("#promtButton").click();
+        
+        
+        
+        
+        // alert will be auto clicked in playwright
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    @Test(description = "playwright fileupload", enabled = false)
+    public void playwrightTestfileupload() {
+        
+        Playwright py = Playwright.create();
+        Browser browser = py.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        
+        BrowserContext bc = browser.newContext();
+        
+        Page p1 = bc.newPage();
+        
+        p1.navigate("https://demoqa.com/automation-practice-form");
+        p1.setInputFiles("input[type='file']", Paths.get("applogin.json"));
+        
+        p1.setInputFiles("input[type='file']", new Path[0]); // this will deselect the file
+        
+        
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+    }
+    
+    @Test(description = "playwright filedownload", enabled = false)
+    public void playwrightTestfileDownload() {
+        
+        Playwright py = Playwright.create();
+        Browser browser = py.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        
+        BrowserContext bc = browser.newContext();
+        
+        Page p1 = bc.newPage();
+        
+        p1.navigate("https://demo.automationtesting.in/FileDownload.html");
+        
+        Download download = p1.waitForDownload(()->{
+            
+            p1.click("button:has-text('Download')");
+        });
+        
+        System.out.println(download.url());
+        
+        System.out.println(download.path());
+        
+        download.saveAs(Paths.get("new path"));
+        
+        download.failure(); // get failure message
+        
+        
+        
+        
+        
+    }
+    
+    @Test(description = "playwright maximize", enabled = false)
+    public void playwrightMaximize() {
+        
+        Playwright py = Playwright.create();
+        Browser browser = py.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        
+        BrowserContext bc = browser.newContext(new Browser.NewContextOptions().setViewportSize(1500, 1500));
+        
+        Page p1 = bc.newPage();
+        
+        p1.navigate("https://demo.automationtesting.in/FileDownload.html");
+     
+        
+        
+    }
+    
+    @Test(description = "playwright screen recording", enabled = false)
+    public void playwrightScreenRecord() {
+        
+        Playwright py = Playwright.create();
+        Browser browser = py.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        
+        BrowserContext bc = browser.newContext(new Browser.NewContextOptions().setRecordVideoDir(Paths.get("myvideo")));
+        
+        Page p1 = bc.newPage();
+        
+        p1.navigate("https://demo.automationtesting.in/FileDownload.html");
+     
+        
+        
+    }
+    
+    @Test(description = "playwright tab window", enabled = true)
+    public void playwrightTabWindow() {
+        
+        Playwright py = Playwright.create();
+        Browser browser = py.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        
+        BrowserContext bc = browser.newContext();
+        
+        Page p1 = bc.newPage();
+        
+        p1.navigate("https://the-internet.herokuapp.com/windows");
+        
+        Page newPage = p1.waitForPopup(()->{
+         
+            p1.click("a[href='/windows/new']");
+        });
+        
+        newPage.waitForLoadState();
+        
+        newPage.navigate("https://the-internet.herokuapp.com/frames");
+        
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+     
+        
+        
+    }
 
 }
